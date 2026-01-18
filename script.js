2ace4d44e10e508191adc04ca7a6d24f40af3cea
kyyaja123 <jawirsalto@gmail.com>
2026-01-18 21:12:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showMessage() {
-    document.getElementById("intro").classList.add("hidden");
-    document.getElementById("message").classList.remove("hidden");
-  }
-  
-  function backToIntro() {
-    document.getElementById("message").classList.add("hidden");
-    document.getElementById("intro").classList.remove("hidden");
-  }
